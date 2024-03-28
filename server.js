100d5395d44d9ea4e3c0102dd6d49d748616520d
sravyaDongari <122468955+sravyaDongari@users.noreply.github.com>
2024-03-28 16:11:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.listen(PORT,()=>{
-  console.log(`Server is running on port ${PORT}`)
-})
 
 
 // Step 2: Database Setup
